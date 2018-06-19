2a2fed75d2c33bd9e75193655584460cab084282
Will Krause <wkrause1@radford.edu>
2018-06-19 14:58:51 +0000

    Reorganized blog posts

--- blog/models.py
+++ blog/models.py
@@ -9,22 +9,24 @@ from wagtail.search import index
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
-
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
 class BlogPage(Page):
     date = models.DateField("Post Date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
-search_fields = Page.search_fields + [
-    index.SearchField('intro'),
-    index.SearchField('body'),
-]
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
 
-content_panels = Page.content_panels + [
-    FieldPanel('date'),
-    FieldPanel('intro'),
-    FieldPanel('body', classname="full"),
-]
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
